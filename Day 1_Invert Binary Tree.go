b5a5461e60093c9e5955020a36747df06e790743
Nafisur <nafisur@MacBook-Air.local>
2020-06-02 00:41:01 +0530

    Initial commit

---
+++ Day 1_Invert Binary Tree.go
@@ -0,0 +1,55 @@
+/*
+Invert Binary Tree
+Solution
+Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+*/
+package leetcode
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+
+	return invert(root)
+}
+
+func invert(node *TreeNode) *TreeNode {
+	if node.Left == nil && node.Right == nil {
+		return node
+	}
+
+	var left *TreeNode
+	if node.Left != nil {
+		left = invert(node.Left)
+	}
+	var right *TreeNode
+	if node.Right != nil {
+		right = invert(node.Right)
+	}
+	node.Left = right
+	node.Right = left
+	return node
+}
