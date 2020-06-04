baee2e7b6e0e157bbda6fb7ce8258527b0760762
Nafisur <nafisur@MacBook-Air.local>
2020-06-04 17:03:32 +0530

    Initial commit

---
+++ Day04_Reverse String.go
@@ -0,0 +1,30 @@
+package leetcode
+
+/*
+Write a function that reverses a string. The input string is given as an array of characters char[].
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+You may assume all the characters consist of printable ascii characters.
+Example 1:
+
+Input: ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+Example 2:
+
+Input: ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+*/
+func reverseString(s []byte) {
+
+	start := 0
+	end := len(s) - 1
+	for start < end {
+		temp := s[start]
+		s[start] = s[end]
+		s[end] = temp
+		start++
+		end--
+	}
+
+}
