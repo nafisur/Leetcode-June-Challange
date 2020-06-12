e7bc08ab6428a612be05ee2c076e4e6d76fb40c6
Nafisur <nafisur@MacBook-Air.local>
2020-06-12 09:09:52 +0530

    Initial commit

---
+++ Day11_Sort Colors.go
@@ -0,0 +1,32 @@
+package leetcode
+
+/*
+Given an array with n objects colored red, white or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white and blue.
+
+Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
+
+Note: You are not suppose to use the library's sort function for this problem.
+
+Example:
+
+Input: [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+*/
+func sortColors(nums []int) {
+
+	var low, mid int
+	high := len(nums) - 1
+
+	for mid <= high {
+		if nums[mid] == 0 {
+			nums[mid], nums[low] = nums[low], nums[mid]
+			low++
+			mid++
+		} else if nums[mid] == 1 {
+			mid++
+		} else {
+			nums[mid], nums[high] = nums[high], nums[mid]
+			high--
+		}
+	}
+}
