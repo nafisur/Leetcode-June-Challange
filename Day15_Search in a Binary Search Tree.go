449f6d2e3e0144893f5a25796db34e617f659c6a
Nafisur <nafisur@MacBook-Air.local>
2020-06-16 11:55:26 +0530

    Initial commit

---
+++ Day15_Search in a Binary Search Tree.go
@@ -0,0 +1,50 @@
+package leetcode
+
+/*
+Given the root node of a binary search tree (BST) and a value.
+You need to find the node in the BST that the node's value equals the given value.
+Return the subtree rooted with that node. If such node doesn't exist, you should return NULL.
+
+For example,
+
+Given the tree:
+        4
+       / \
+      2   7
+     / \
+    1   3
+
+And the value to search: 2
+You should return this subtree:
+
+      2
+     / \
+    1   3
+In the example above, if we want to search the value 5, since there is no node with value 5,
+we should return NULL.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func searchBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return root
+	}
+
+	if root.Val == val {
+		return root
+	}
+	if root.Val > val {
+		return searchBST(root.Left, val)
+	}
+	if root.Val < val {
+		return searchBST(root.Right, val)
+	}
+	return nil
+}
