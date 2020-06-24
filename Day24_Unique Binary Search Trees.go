3653cf2adedcc05a7a1b5f14d386b2c5c6ccd32a
Nafisur <nafisur@MacBook-Air.local>
2020-06-24 13:35:46 +0530

    Initial commit

---
+++ Day24_Unique Binary Search Trees.go
@@ -0,0 +1,35 @@
+package leetcode
+
+/*
+Given n, how many structurally unique BST's (binary search trees) that store values 1 ... n?
+
+Example:
+
+Input: 3
+Output: 5
+Explanation:
+Given n = 3, there are a total of 5 unique BST's:
+
+   1         3     3      2      1
+    \       /     /      / \      \
+     3     2     1      1   3      2
+    /     /       \                 \
+   2     1         2                 3
+*/
+
+func numTrees(n int) int {
+	if n < 2 {
+		return n
+	}
+	dp := make([]int, n+1)
+	dp[0] = 1
+	dp[1] = 1
+	for i := 2; i <= n; i++ {
+		for j := 1; j <= i; j++ {
+
+			dp[i] += dp[j-1] * dp[i-j]
+		}
+	}
+
+	return dp[n]
+}
