9521c768c26c738d8cb7708785624c1d8e5d3e7a
Nafisur <nafisur@nafisur.pune.cdac.in>
2020-06-30 14:33:34 +0530

    Initial commit

---
+++ Day30_Word Search II.go
@@ -0,0 +1,70 @@
+package leetcode
+
+/*
+Given a 2D board and a list of words from the dictionary, find all words in the board.
+
+Each word must be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once in a word.
+
+
+
+Example:
+
+Input:
+board = [
+  ['o','a','a','n'],
+  ['e','t','a','e'],
+  ['i','h','k','r'],
+  ['i','f','l','v']
+]
+words = ["oath","pea","eat","rain"]
+
+Output: ["eat","oath"]
+*/
+type TrieNode struct {
+	word     string
+	children [26]*TrieNode
+}
+
+func findWords(board [][]byte, words []string) []string {
+	root := &TrieNode{}
+	for _, w := range words {
+		node := root
+		for _, c := range w {
+			if node.children[c-'a'] == nil {
+				node.children[c-'a'] = &TrieNode{}
+			}
+			node = node.children[c-'a']
+		}
+		node.word = w
+	}
+
+	result := make([]string, 0)
+	for i := 0; i < len(board); i++ {
+		for j := 0; j < len(board[0]); j++ {
+			dfs(i, j, board, root, &result)
+		}
+	}
+	return result
+}
+
+func dfs(i, j int, board [][]byte, node *TrieNode, result *[]string) {
+	if i < 0 || j < 0 || i == len(board) || j == len(board[0]) {
+		return
+	}
+	c := board[i][j]
+	if c == '#' || node.children[c-'a'] == nil {
+		return
+	}
+	node = node.children[c-'a']
+	if node.word != "" {
+		*result = append(*result, node.word)
+		node.word = ""
+	}
+
+	board[i][j] = '#'
+	dfs(i+1, j, board, node, result)
+	dfs(i-1, j, board, node, result)
+	dfs(i, j+1, board, node, result)
+	dfs(i, j-1, board, node, result)
+	board[i][j] = c
+}
